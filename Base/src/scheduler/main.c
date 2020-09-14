b246b842c17ad3bf0ea40982a39aa3f33995b9ae
Maria Luisa Claro <muclaro@uc.cl>
2020-09-14 17:21:34 -0300

    not_started_processes

--- Base/src/scheduler/main.c
+++ Base/src/scheduler/main.c
@@ -9,7 +9,7 @@
 #include <errno.h>
 #include <signal.h>
 
-void read_input(char* filename)
+Queue* read_input(char* filename)
 {
   // Abrimos el archivo
   FILE* file = fopen(filename, "r");
@@ -17,9 +17,9 @@ void read_input(char* filename)
   // Leemos la cantidad de procesos
   int total_processes;
   fscanf(file, "%i", &total_processes);
-  printf("Numero de procesos: %i\n", total_processes);
 
-  Process processes[total_processes];
+  Queue* queue = queue_init();
+
 
   char line[256];
 
@@ -28,51 +28,48 @@ void read_input(char* filename)
   // Inicializar cada proceso
   for (int p = 0; p < total_processes; p++)
   {
-
+    Process* process = malloc(sizeof(Process));
+    process -> next = NULL;
     fgets(line, sizeof(line), file);
-    printf("Proceso numero %i: %s\n", p, line);
 
     const char s[2] = " ";
     char *token;
 
     /* get the process name */
     token = strtok(line, s);
-    strcpy(processes[p].name, token);
-    printf( "Nombre: %s\n", processes[p].name);
+    strcpy(process->name, token);
 
     /* get the pid */
     token = strtok(NULL, s);
     char pid_str[255];
     strcpy(pid_str, token);
     int pid = atoi(pid_str);
-    processes[p].pid = pid;
-    printf( "PID: %i\n", processes[p].pid);
+    process->pid = pid;
+
 
     /* get the start time */
     token = strtok(NULL, s);
     char start_time_str[255];
     strcpy(start_time_str, token);
     int start_time = atoi(start_time_str);
-    processes[p].start_time = start_time;
-    printf( "Start time: %i\n", processes[p].start_time);
+    process->start_time = start_time;
+ 
 
     /* get the deadline */
     token = strtok(NULL, s);
     char deadline_str[255];
     strcpy(deadline_str, token);
     int deadline = atoi(deadline_str);
-    processes[p].deadline = deadline;
-    printf( "Deadline: %i\n", processes[p].deadline);
+    process->deadline= deadline;
 
     /* get the bursts */
     token = strtok(NULL, s);
     char bursts_str[255];
     strcpy(bursts_str, token);
     int bursts = atoi(bursts_str);
-    processes[p].total_bursts = bursts;
-    printf( "Total bursts: %i\n", processes[p].total_bursts);
+    process->total_bursts = bursts;
 
-    for (int b = 0; b < (processes[p].total_bursts * 2) - 1; b++)
+    for (int b = 0; b < (bursts * 2) - 1; b++)
     {
       token = strtok(NULL, s);
       char burst_str[255];
@@ -81,21 +78,17 @@ void read_input(char* filename)
       printf("b: %i, burst: %i\n", b, burst);
 
       if(b % 2 == 0) {
-        processes[p].cpu_bursts[b/2] = burst;
+        process->cpu_bursts[b/2] = burst;
       } 
       else {
-        processes[p].io_bursts[b/2] = burst; 
+        process->io_bursts[b/2] = burst; 
       }
     }
     
-    for (int b = 0; b < processes[p].total_bursts - 1; b++)
-    {
-      printf("CPU burst %i: %i\n", b, processes[p].cpu_bursts[b]);
-      printf("IO burst %i: %i\n", b, processes[p].io_bursts[b]);
-    }
-    
+    list_start_time_append(queue->not_started_processes, process);
   }
   fclose(file);
+  return queue;
 }
 
 int main(int argc, char *argv[])
@@ -107,13 +100,13 @@ int main(int argc, char *argv[])
   {
     int cpus = 1;
     printf("CPUs: %i", cpus);
-    read_input(argv[1]);
+    Queue* queue =read_input(argv[1]);
   }
   else if (argc == 4)
   {
     int cpus = atoi(argv[3]);
     printf("CPUs: %i", cpus);
-    read_input(argv[1]); 
+    Queue* queue = read_input(argv[1]); 
     
   }
   else
@@ -122,8 +115,6 @@ int main(int argc, char *argv[])
     printf("Uso correcto: %s PATH_TO_INPUT\n", argv[0]);
     return 1;
   }
-
-  
 
   return 0;
 }
